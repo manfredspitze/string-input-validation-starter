8d5af3009f1aea88d52b6fc348964f1c5dcffd4e
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-11-22 15:04:38 -0500

    Initial commit

---
+++ examples/solution_code.py
@@ -0,0 +1,34 @@
+# SOLUTION CODE
+# Project title
+
+# Activity 1
+# Comment
+# Comment
+
+
+
+
+# Activity 2
+# Comment
+# Comment
+
+
+
+
+# Activity 3
+# Comment
+# Comment
+
+
+
+
+# Activity 4
+# Comment
+# Comment
+
+
+
+
+# Part 5
+# Comment
+# Comment
